f70ebe24b9f6ea9fcbf6b1bafaf147554bc06501
jessicapaola <jessicapaolabm@gmail.com>
2021-11-04 19:36:24 -0300

    Correçao do metodo print para exibir a saida correta no algoritmo guloso

--- algoritmo/AlgoritmoGuloso.py
+++ algoritmo/AlgoritmoGuloso.py
@@ -41,7 +41,7 @@ class AGuloso:
 
     def print(self):
         while not self.__queue.empty():
-            print("{} ".format(self.__queue))
+            print("{} ".format(self.__queue.get()), end="")
         print()
     
     def search(self, root, destiny):
